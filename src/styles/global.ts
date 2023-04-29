411ee64e48723b00771c158b26a3b5d32dd68a5f
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-04-29 15:08:38 -0400

    Initial commit

---
+++ src/styles/global.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	:focus {
+		outline: none;
+		box-shadow: 0 0 0 2px ${({ theme }) => theme['blue']};
+	}
+
+	body {
+		background-color: ${({ theme }) => theme['base-background']};
+		color: ${({ theme }) => theme['base-text']};
+		-webkit-font-smoothing: antialiased;
+	}
+
+	body, input, textarea, button {
+		font: 400 1rem 'Nunito', sans-serif;
+	}
+
+	/* width */
+	::-webkit-scrollbar {
+		width: 8px;
+	}
+
+	/* Track */
+	::-webkit-scrollbar-track {
+		background: #f1f1f1;
+	}
+
+	/* Handle */
+	::-webkit-scrollbar-thumb {
+		background: #888;
+		border-radius: 999px;
+	}
+
+	/* Handle on hover */
+	::-webkit-scrollbar-thumb:hover {
+		background: #555;
+}`;
